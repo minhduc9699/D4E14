844b856b5d5233a5bd5590760e87e0a319ffdd69
Phạm Đức <minhduc.096.99@gmail.com>
2020-07-20 08:58:38 +0700

    add hw solution

---
+++ session2/hw/con_rua_again.py
@@ -0,0 +1,18 @@
+
+from turtle import *
+
+shape('turtle')
+color('red')
+
+right(30)
+for i in range(4):
+  forward(50)
+  left(60)
+  forward(50)
+  left(120)
+  forward(50)
+  left(60)
+  forward(50)
+  left(210)
+
+mainloop()
---
+++ session2/hw/multiplication_table2.py
@@ -0,0 +1,10 @@
+num = int(input('enter a number:'))
+
+
+for row in range (num):
+    for column in range(num):
+      if (row + column) % 2 == 0:
+        print(1, end='  ')
+      else:
+        print(0, end='  ')
+    print('\n')
