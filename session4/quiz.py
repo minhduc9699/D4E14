6bfa315ff1ed45dbc8dec1a02416869a45b4e28e
Phạm Đức <minhduc.096.99@gmail.com>
2020-07-21 21:47:57 +0700

    session4

--- session4/quiz.py
+++ session4/quiz.py
@@ -1,32 +1,19 @@
 import pyexcel
 
-quizzes = [
-  {
-    'question': 'con nhện có mấy chân?',
-    'choices': [3, 4, 5, 6],
-    'right_choice': 3
-  },
-  {
-    'question': 'con chó có mấy chân?',
-    'choices': [3, 4, 5, 6],
-    'right_choice': 1
-  }, 
-  {
-    'question': 'con chó có mấy chân?',
-    'choices': [3, 4, 5, 6],
-    'right_choice': 1
-  }
-]
+quizzes = pyexcel.iget_records(file_name='quizzes.xlsx')
 
 right_choices_count = 0
 
+quizzes = list(quizzes)
 for quiz in quizzes:
+  quiz['choices'] = quiz['choices'].split(',')
+
   print(quiz['question'])
   for i in range(len(quiz['choices'])):
     print(f' {i+1}. {quiz["choices"][i]} ')
 
   your_choice = int(input('your choice: ')) - 1
-  if your_choice == quiz['right_choice']:
+  if your_choice == int(quiz['right_choice']):
     right_choices_count = right_choices_count + 1
   else:
     print('sai mất r :((')
