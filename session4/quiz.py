1c5bd47cbc875fe4d65c5eb60a4b5a52ebd13ac3
Phạm Đức <minhduc.096.99@gmail.com>
2020-07-21 20:49:18 +0700

    session4

--- session4/quiz.py
+++ session4/quiz.py
@@ -1,9 +1,23 @@
-quiz = {
-  'question': 'con nhện có mấy chân?',
-  'choices': [3, 4, 5, 6],
-  'right_choice': 3
-}
+quizzes = [
+  {
+    'question': 'con nhện có mấy chân?',
+    'choices': [3, 4, 5, 6],
+    'right_choice': 3
+  },
+  {
+    'question': 'con chó có mấy chân?',
+    'choices': [3, 4, 5, 6],
+    'right_choice': 1
+  }
+]
 
-print(quiz['question'])
-for i in range(len(quiz['choices'])):
-  print(f' {i+1}. {quiz["choices"][i]} ')
+for quiz in quizzes:
+  print(quiz['question'])
+  for i in range(len(quiz['choices'])):
+    print(f' {i+1}. {quiz["choices"][i]} ')
+
+  your_choice = int(input('your choice: ')) - 1
+  if your_choice == quiz['right_choice']:
+    print('hurayy!')
+  else:
+    print('sai mất r :((')
