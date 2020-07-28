f59d6bb077a4f7bedba0d5ab09d6c2db8b5891f2
Phạm Đức <minhduc.096.99@gmail.com>
2020-07-28 21:40:52 +0700

    session5

--- session5/mongo_intro.py
+++ session5/mongo_intro.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from bson import ObjectId
 
 connection = MongoClient() # connect to mongodb
 
@@ -7,7 +8,21 @@ quiz_database = connection.get_database('quiz')
 
 result_collection = quiz_database.get_collection('result')
 new_collection = new_database.get_collection('students') # create / get collection
+quiz_collection = quiz_database.get_collection('quizzes')
 
 # new_collection.insert_one({'name': 'first data'}) # write document to collection
 result_found = result_collection.find({'name': {'$ne': 'Đức'} })
 print(list(result_found))
+
+quiz_collection.update_one(
+    {'_id': ObjectId("5f202aad945100442014fc50")},  # query
+    {
+        '$push': {'choices': "7"} # push, set, unset
+    } # update
+)
+
+result_collection.delete_one(
+    {
+        'correct_percent': 100 
+    }
+) # Delete
