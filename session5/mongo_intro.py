f1fe231dc0bb4a10298684a8fe45fa3fa1eac96b
Phạm Đức <minhduc.096.99@gmail.com>
2020-07-28 21:06:11 +0700

    session5

--- session5/mongo_intro.py
+++ session5/mongo_intro.py
@@ -3,7 +3,11 @@ from pymongo import MongoClient
 connection = MongoClient() # connect to mongodb
 
 new_database = connection.get_database('D4E15') # create / get database
+quiz_database = connection.get_database('quiz')
 
+result_collection = quiz_database.get_collection('result')
 new_collection = new_database.get_collection('students') # create / get collection
 
-new_collection.insert_one({'name': 'first data'}) # write document to collection
+# new_collection.insert_one({'name': 'first data'}) # write document to collection
+result_found = result_collection.find({'name': {'$ne': 'Đức'} })
+print(list(result_found))
